992d57f328923c0f74aa0a4d1d76fa9a57a5903d
shilpi-mahi <103402550+shilpi-mahi@users.noreply.github.com>
2022-04-12 13:30:57 +0530

    Initial commit

---
+++ GF.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+main()
+{
+    int gm="DETECT",gd;
+    initgraph(&GD,&GM")
+    line(100,200,190,200);
+    getch();
+    return(0);
+    closegraph  
+}
---
+++ feed.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>BPMS||Home Page</title>
+        
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+	  <?php include_once('includes/header.php');?>
